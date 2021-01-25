4f123029e119142b3e9e6626142b9598a6d26212
ashishb888 <ashish.bhosle008@gmail.com>
2021-01-25 17:43:25 +0530

    produce-json-xml: Initial commit

---
+++ produce-json-xml/src/main/java/poc/springboot/ProduceJsonXmlApplication.java
@@ -0,0 +1,13 @@
+package poc.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduceJsonXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProduceJsonXmlApplication.class, args);
+	}
+
+}
---
+++ produce-json-xml/src/test/java/poc/springboot/ProduceJsonXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package poc.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProduceJsonXmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
