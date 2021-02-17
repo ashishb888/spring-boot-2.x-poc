b1be8d5d2b1b1a2631fd03957847131316456945
ashishb888 <ashish.bhosle008@gmail.com>
2021-02-17 20:45:56 +0530

    java11-and-gradle: Initial commit

---
+++ java11-and-gradle/src/main/java/poc/springboot/Java11AndGradleApplication.java
@@ -0,0 +1,13 @@
+package poc.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java11AndGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java11AndGradleApplication.class, args);
+	}
+
+}
